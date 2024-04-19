f041bd4314e63fb97a6b5d9ddbc68ad6835d5879
ProjectsAssesment <kumaramaduka5@gmail.com>
2024-04-20 04:26:43 +0530

    add setup

---
+++ src/pages/Authentication/Authentication.jsx
@@ -0,0 +1,16 @@
+import { Grid } from '@mui/material'
+import React from 'react'
+
+const Authentication = () => {
+  return (
+    <div>
+      <Grid container>
+        <Grid item xs={7}>
+            <img className='h-full w-full' src=''/>
+        </Grid>
+      </Grid>
+    </div>
+  )
+}
+
+export default Authentication
