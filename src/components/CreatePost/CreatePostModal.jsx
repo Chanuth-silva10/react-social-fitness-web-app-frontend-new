a42d661499b660de9c0b41049f541c1fdb449814
ProjectsAssesment <kumaramaduka5@gmail.com>
2024-04-25 12:05:56 +0530

    implement post create

--- src/components/CreatePost/CreatePostModal.jsx
+++ src/components/CreatePost/CreatePostModal.jsx
@@ -1,39 +1,135 @@
-import { Box, Modal, Typography } from '@mui/material'
-import React from 'react'
+import {
+  Avatar,
+  Backdrop,
+  Box,
+  Button,
+  CircularProgress,
+  IconButton,
+  Modal,
+  Typography,
+} from "@mui/material";
+import { useFormik } from "formik";
+import React, { useState } from "react";
+import ImageIcon from "@mui/icons-material/Image";
+import VideoIcon from "@mui/icons-material/VideoCall";
 
 const style = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: 500,
-    bgcolor: 'background.paper',
-    border: '2px solid #000',
-    boxShadow: 24,
-    p: 4,
-    borderRadious: ".6rem",
-    outline: "none",
-  };
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 500,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+  borderRadious: ".6rem",
+  outline: "none",
+};
 
+const CreatePostModal = ({ handleClose, open }) => {
+  const [selectedImage, setSelectedImage] = useState();
+  const [selectedVideo, setSelectedVideo] = useState();
+  const [isLoading, setIsLoading] = useState(false);
 
-const CreatePostModal = ({handleClose,open}) => {
+  const handleSelectImage = () => {};
+
+  const handleSelectVideo = () => {};
+
+  const formik = useFormik({
+    initialValues: {
+      caption: "",
+      image: "",
+      video: "",
+    },
+    onSubmit: (values) => {},
+  });
+
   return (
     <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Text in a modal
-          </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </Typography>
-        </Box>
-      </Modal>
-  )
-}
+      open={open}
+      onClose={handleClose}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <Box sx={style}>
+        <form onSubmit={formik.handleSubmit}>
+          <div>
+            <div className="flex space-x-4 items-center">
+              <Avatar />
+              <div>
+                <p className="font-bold text-lg">Chanuth with Social</p>
+                <p className="text-sm">@Chanuth</p>
+              </div>
+            </div>
+            <textarea
+              className="outline-none w-full mt-5 p-2 
+              bg-transparent border border-[#3b4054] rounded-sm"
+              placeholder="write caption..."
+              name="caption"
+              id=""
+              onChange={formik.handleChange}
+              value={formik.values.caption}
+              rows="4"
+            ></textarea>
+            <div className="flex space-x-5 items-center mt-5">
+              <div>
+                <input
+                  type="file"
+                  accept="image/*"
+                  onChange={handleSelectImage}
+                  style={{ display: "none" }}
+                  id="image-input"
+                />
+                <label htmlFor="image-input">
+                  <IconButton color="primary">
+                    <ImageIcon />
+                  </IconButton>
+                </label>
+                <span>Image</span>
+              </div>
+              <div>
+                <input
+                  type="file"
+                  accept="video/*"
+                  onChange={handleSelectVideo}
+                  style={{ display: "none" }}
+                  id="video-input"
+                />
+                <label htmlFor="video-input">
+                  <IconButton color="primary">
+                    <VideoIcon />
+                  </IconButton>
+                </label>
+                <span>Video</span>
+              </div>
+            </div>
+            {selectedImage && (
+              <div>
+                <img className="h-[10rem]" src={selectedImage} alt="" />
+              </div>
+            )}
+            <div className="flex w-full justify-end">
+              <Button
+                variant="contained"
+                type="submit"
+                sx={{ borderRadius: "1.5rem" }}
+              >
+                Post
+              </Button>
+            </div>
+          </div>
+        </form>
+        <Backdrop
+          sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
+          open={isLoading}
+          onClick={handleClose}
+        >
+          <CircularProgress color="inherit" />
+        </Backdrop>
+      </Box>
+    </Modal>
+  );
+};
 
-export default CreatePostModal
+export default CreatePostModal;
--- src/components/Middelpart/Middelpart.jsx
+++ src/components/Middelpart/Middelpart.jsx
@@ -1,16 +1,22 @@
 import { Avatar, Card, IconButton } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
 import AddIcon from "@mui/icons-material/Add";
 import StoryCircle from "./StoryCircle";
 import ImageIcon from "@mui/icons-material/Image";
 import VideocamIcon from "@mui/icons-material/Videocam";
 import ArticleIcon from "@mui/icons-material/Article";
 import PostCard from "../Post/PostCard";
+import CreatePostModal from "../CreatePost/CreatePostModal";
 
 const story = [11, 1, 1, 1, 1];
 const posts = [1, 1, 1, 1, 1];
 const Middelpart = () => {
+  const [openCreatePostModal, setOpenCreatePostModal] = useState();
+  const handleCloseCreatePostModal=()=>setOpenCreatePostModal(false);
+
+
   const handleOpenCreatePostModel = () => {
+    setOpenCreatePostModal(true);
     console.log("Open the post model");
   };
 
@@ -31,6 +37,8 @@ const Middelpart = () => {
         <div className="flex justify-between">
           <Avatar />
           <input
+            onClick={handleOpenCreatePostModel}
+            readOnly
             className="outline-none w-[90%] rounded-full px-5 bg-transparent border-[#3b4054] border"
             type="text"
           />
@@ -59,6 +67,12 @@ const Middelpart = () => {
       <div className="mt-5 space-y-5">
         {posts.map((item)=><PostCard />)}
       </div>
+
+      
+      <div>
+        <CreatePostModal handleClose={handleCloseCreatePostModal} open={openCreatePostModal}/>
+      </div>
+
     </div>
   );
 };
